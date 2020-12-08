e717d3fe5e1b98e9a7f11fa707aced00bb9f9d2a
Sergey <vov4ok2000@yandex.ru>
2020-12-08 17:12:15 +0300

    new problems

--- 4.js
+++ 4.js
@@ -12,3 +12,21 @@
 
 // let grid = [[-1]];
 // console.log(countNegatives(grid));
+
+// var arrayPairSum = function(nums) {
+//     let arr = [], sum = 0;
+
+//     for(let i = 0; i < nums.length; i += 2){
+//         if(nums[i]<nums[i+1]) arr.push(nums[i]);
+//         else arr.push(nums[i+1]);
+//     }
+
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     console.log(arr);
+//     return sum;
+// };
+
+// let nums = [6,2,6,5,1,2];
+// console.log(arrayPairSum(nums));
